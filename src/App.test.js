32a5a012cbc1d934bb3b3ffd47543fa176465c7b
Jon Q <itsjonq@gmail.com>
2018-09-21 16:34:15 -0400

    Add enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mount } from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = mount(<App />)
+
+  expect(wrapper.find('div').length).toBeGreaterThanOrEqual(1)
 });
