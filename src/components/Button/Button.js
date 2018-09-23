caadc1654becec7507bd4a22cb163eda369f1929
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2018-09-23 14:14:51 -0700

    Adds PropTypes and default Props for Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,1 +1,32 @@
-// Create the new Button component here!
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class Button extends Component {
+  static propTypes = {
+    size: PropTypes.oneOf(['small', 'normal', 'large']),
+    block: PropTypes.bool,
+    type: PropTypes.oneOf(['default', 'primary', 'success', 'error']),
+    disabled: PropTypes.bool,
+    status: PropTypes.oneOf(['loading', 'success', 'error']),
+    href: PropTypes.string,
+    target: PropTypes.string,
+    linkComponent: PropTypes.element,
+  }
+
+  static defaultProps = {
+    size: 'normal',
+    block: false,
+    type: 'default',
+    disabled: false,
+    status: undefined,
+    href: '',
+    target: '',
+    linkComponent: undefined,
+  }
+
+  render() {
+    return (
+    );
+  }
+}
+
